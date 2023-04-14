ef6ca747d17d4643f746484ae52653b80afccee9
Alberto Lee <a.lee@malms.aero>
2023-04-14 19:18:47 +0100

    CLASS

--- connect4.py
+++ connect4.py
@@ -1,25 +1,70 @@
 
-board = [[0]*7]*6
 
-print(board)
 
-def place_chip(pos, playerVal):
-    placePos = 0
-    for i in range(0, 5):
-        square = board[i][pos]
-        if(square != 0):
-            placePos = i-1
-    if(placePos != -1)
-        board[placePos][i] = playerVal
-        return True
-    else:
-        return False
+class Connect4:
+    def __init__(self):
+        self.board = [[0]*7]*6
 
-def check_for_win(x, y):
-    win = True
-    check_x = x
-    check_y = y
-    while(win):
-        if(check_y >= 0 and check_x >= 0 and check_y <= 5 and check_x <= 6):
-            board[check_y][check_x] = 
+    
+    def place_chip(pos, playerVal):
+        placePos = 0
+        for i in range(0, 5):
+            square = board[i][pos]
+            if(square != 0):
+                placePos = i-1
+        if(placePos != -1)
+            board[placePos][i] = playerVal
+            return True
+        else:
+            return False
+
+    def check_for_win(x, y, playerVal):
+        were_good_counter = 0
+        
+        for d in range(0, 7):
+            for i in range(0, 3):
+                check_y = y 
+                check_x = x
+
+                #west
+                if(d == 0):
+                    check_x = check_x - i
+                #east
+                if(d == 1):
+                    check_x = check_x + i
+                #north
+                if(d == 2):
+                    check_y = check_y - i
+                #south
+                if(d == 3):
+                    check_y = check_y + i
+                #northwest
+                if(d == 4):
+                    check_y = check_y - i
+                    check_x = check_x - i
+                #northeast:
+                if(d == 5):
+                    check_y = check_y - i
+                    check_x = check_x + i
+                #southeast:
+                if(d == 6):
+                    check_y = check_y + i
+                    check_x = check_x + i
+                #southwest
+                if(d == 7):
+                    check_y = check_y - i
+                    check_x = check_x - i
+                
+                if(check_y >= 0 and check_x >= 0 and check_y <= 5 and check_x <= 6):
+                    if(board[check_y][check_x] == playerVal):
+                        were_good_counter += 1
+                    else:
+                        break
+                    
+                if(were_good_counter == 4):
+                    return True
+        
+
+
+
 
